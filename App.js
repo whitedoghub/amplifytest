067c525d3e950ef2d7b64475fa3e9a1565d1cacb
kbk109 <kbk109@gmail.com>
2020-08-17 16:16:21 +0900

    init

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react';
+import {Button, Linking, Text, View} from 'react-native';
+import Amplify, {Auth, Hub} from 'aws-amplify';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
+import awsconfig from './aws-exports';
+
+async function urlOpener(url, redirectUrl) {
+  await InAppBrowser.isAvailable();
+  const {type, url: newUrl} = await InAppBrowser.openAuth(url, redirectUrl, {
+    showTitle: false,
+    enableUrlBarHiding: true,
+    enableDefaultShare: false,
+    ephemeralWebSession: false,
+  });
+
+  if (type === 'success') {
+    Linking.openURL(newUrl);
+  }
+}
+
+Amplify.configure({
+  ...awsconfig,
+  oauth: {
+    ...awsconfig.oauth,
+    urlOpener,
+  },
+});
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({payload: {event, data}}) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then((userData) => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then((userData) => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
+  return (
+    <View>
+      <Text>User: {user ? JSON.stringify(user.attributes) : 'None'}</Text>
+      {user ? (
+        <Button title="Sign Out" onPress={() => Auth.signOut()} />
+      ) : (
+        <Button
+          title="Federated Sign In"
+          onPress={() => Auth.federatedSignIn({provider: 'Google'})}
+        />
+      )}
+    </View>
+  );
+}
+
+export default App;
